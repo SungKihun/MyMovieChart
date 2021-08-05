2a88a2db94bff38ab535b57e04166bdc3e34a0ea
SungKihun <tjdrlgns3293@gmail.com>
2021-08-05 16:09:34 +0900

    Custom Class Complete

--- MyMovieChart/MovieCell.swift
+++ MyMovieChart/MovieCell.swift
@@ -9,4 +9,11 @@ import UIKit
 
 class MovieCell: UITableViewCell {
     
+    @IBOutlet var title: UILabel! // 영화제목
+    
+    @IBOutlet var opendate: UILabel! // 영화 설명
+    
+    @IBOutlet var rating: UILabel! // 개봉일
+    
+    @IBOutlet var desc: UILabel! // 평점
 }
