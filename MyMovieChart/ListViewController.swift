868669e3b50a675aecce4d14da60065a1bbdc9e3
SungKihun <tjdrlgns3293@gmail.com>
2021-08-14 08:36:18 +0900

    api 호출 데이터 30개로 조정

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -40,7 +40,7 @@ class ListViewController: UITableViewController {
     
     func callMovieAPI() {
         // 1. 호핀 API 호출을 위한 URI를 생성
-        let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=\(self.page)&count=10&genreId=&order=releasedateasc"
+        let url = "http://swiftapi.rubypaper.co.kr:2029/hoppin/movies?version=1&page=\(self.page)&count=30&genreId=&order=releasedateasc"
         let apiURI: URL! = URL(string: url)
         
         // 2. REST API를 호출
@@ -48,39 +48,39 @@ class ListViewController: UITableViewController {
         
         // 3. 데이터 전송 결과를 로그로 출력 (반드시 필요한 코드는 아님)
         let log = NSString(data: apidata, encoding: String.Encoding.utf8.rawValue) ?? "데이터가 없습니다"
-        NSLog("API Result = \(log)")
+//        NSLog("API Result = \(log)")
         
         // 4. JSON 객체를 파싱하여 NSDictionary 객체로 변환
         do {
             let apiDictionary = try JSONSerialization.jsonObject(with: apidata, options: []) as! NSDictionary
-            
+            NSLog("API Result = \(apiDictionary)")
             // 5. 데이터 구조에 따라 차례대로 캐스팅하며 읽어온다.
             let hoppin = apiDictionary["hoppin"] as! NSDictionary
             let movies = hoppin["movies"] as! NSDictionary
             let movie = movies["movie"] as! NSArray
-            
+
             // 6. Iterator 처리를 하면서 API 데이터를 MovieVO 객체에 저장한다.
             for row in movie {
                 // 순회 상수를 NSDictionary 타입으로 캐스팅
                 let r = row as! NSDictionary
-                
+
                 // 테이블 뷰 리스트를 구성할 데이터 형싱
                 let mvo = MovieVO()
-                
+
                 // movie 배열의 각 데이터를 mvo 상수의 속성에 대입
                 mvo.title       = r["title"] as? String
                 mvo.description = r["genreNames"] as? String
                 mvo.thumbnail   = r["thumbnailImage"] as? String
                 mvo.detail      = r["linkUrl"] as? String
                 mvo.rating      = ((r["ratingAverage"] as! NSString).doubleValue)
-                
+
                 // list 배열에 추가
                 self.list.append(mvo)
             }
-            
+
             // 7. 전체 데이터 카운트를 얻는다.
             let totalCount = (hoppin["totalCount"] as? NSString)!.integerValue
-             
+
             // 8. totalCount가 읽어온 데이터 크기와 같거나 클 경우 더보기 버튼을 막는다.
             if (self.list.count >= totalCount) {
                 self.moreBtn.isHidden = true
@@ -97,6 +97,10 @@ class ListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 주어진 행에 맞는 데이터 소스를 읽어온다.
         let row = self.list[indexPath.row]
+        
+        // 로그 출력
+        NSLog("제목: \(row.title!), 호출된 행번호: \(indexPath.row)")
+        
         // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell") as! MovieCell
         
