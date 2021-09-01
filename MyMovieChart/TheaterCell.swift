2acc555b4b440e34041f37e66aa9bbde3ac9f28b
SungKihun <tjdrlgns3293@gmail.com>
2021-09-01 21:37:09 +0900

    TheaterListController 영화관 정보 읽어오기

---
+++ MyMovieChart/TheaterCell.swift
@@ -0,0 +1,18 @@
+//
+//  TheaterCell.swift
+//  MyMovieChart
+//
+//  Created by 성기훈 on 2021/09/01.
+//
+
+import UIKit
+
+class TheaterCell: UITableViewCell {
+    
+    // 상영관명
+    @IBOutlet var name: UILabel!
+    // 연락처
+    @IBOutlet var tel: UILabel!
+    // 주소
+    @IBOutlet var addr: UILabel!
+}
